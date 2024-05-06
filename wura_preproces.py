1504bf6eb1a7182a6e338466f6284f73dbc8cbc0
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-05 21:09:46 -0400

    modified file

--- wura_preproces.py
+++ wura_preproces.py
@@ -4,7 +4,7 @@ import os
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
-dataset = load_dataset("castorini/wura", "fra", level="passage", verification_mode="no_checks")
+dataset = load_dataset("castorini/wura", "arz", level="passage", verification_mode="no_checks")
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", token='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')
 
@@ -36,7 +36,7 @@ def tokenize_data(dataset):
 tokenized_dataset = tokenize_data(dataset)
 
 
-output_file = "fra_tokenized.jsonl"
+output_file = "arz_tokenized.jsonl"
 
 if isinstance(tokenized_dataset, dict):
     for split_name, split_data in tokenized_dataset.items():
