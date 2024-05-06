1474a335ffacd220ce3f8eaaaad80247fed2abf5
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-06 12:23:52 -0400

    modified file

---
+++ max_len.py
@@ -0,0 +1,8 @@
+import json
+
+with open('wura_data/train/train_afr_tokenized.jsonal') as f:
+  data = [json.loads(line) for line in f]
+
+  # Add parentheses around the generator expression
+
+  print(max(d['length'] for d in data))
