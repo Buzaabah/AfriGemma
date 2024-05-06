67467b4e00a35634af980799655e378315267fd0
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-06 12:26:03 -0400

    modified file

--- max_len.py
+++ max_len.py
@@ -1,6 +1,6 @@
 import json
 
-with open('wura_data/train/train_afr_tokenized.jsonal') as f:
+with open('wura_data/train/train_afr_tokenized.jsonl') as f:
   data = [json.loads(line) for line in f]
 
   # Add parentheses around the generator expression
