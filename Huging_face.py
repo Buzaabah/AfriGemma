d56e5484c11df5c9a23edf21ae8e8759dcb2b710
Happy Buzaaba <hbuzaaba@gmail.com>
2024-07-24 15:17:39 -0400

    added new file

--- Huging_face.py
+++ Huging_face.py
@@ -20,13 +20,21 @@ print(f"Current token: {token}")
 # directory = "../../awettig/scaling_wura/checkpoints/"
 filepath = "../../awettig/scaling_wura/checkpoints/Meta-Llama-3-8B_wura_data-packed_bsz256_steps3000_lr6e-5_warmup0.05_afr+amh+eng+fra+hau+ibo+kin+mlg+nya+orm+por+sna+som+sot+swa+tir+xho+yor+zul"
 
+if os.path.isfile(filepath):
+    print("File exists!")
+else:
+    print("File does not exist. Check the path.")
+
 # for filename in os.listdir(directory):
 #    filepath = os.path.join(directory, filename)
 #    if os.path.isfile(filepath):
 
+"""
 api.upload_file(
     path_or_fileobj=filepath,
     path_in_repo="Meta-Llama-3-8B_wura_data-packed_bsz256_steps3000_lr6e-5_warmup0.05_afr+amh+eng+fra+hau+ibo+kin+mlg+nya+orm+por+sna+som+sot+swa+tir+xho+yor+zul",
     repo_id="Happyb/afroMeta-Llama-3-8B",
     token=token
 )
+
+"""
